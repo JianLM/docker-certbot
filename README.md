f320bbfa36d89f04e77066f8284a0287e4dc4dd3
11notes <github@11notes.ch>
2018-09-06 16:22:14 +0200

    certbot

---
+++ README.md
@@ -0,0 +1,8 @@
+# docker-certbot
+
+Container that redirects all HTTP traffic to HTTPS and will create lets encrypt certificates to be exported and read from other containers or systems
+
+## build with
+
+* [Alpine Linux](https://alpinelinux.org/) - Alpine Linux
+* [Docker](https://www.docker.com/) - Docker Container System
---
+++ source/certbot.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+CB_NAME=/var/ssl/"$1"
+mkdir -p $CB_NAME
+echo "creating certificate for $3"
+certbot certonly --cert-name "$1" --non-interactive --agree-tos --email "$2" --keep --webroot -w /var/www $3
+cp /etc/letsencrypt/live/"$1" -RL /var/ssl
+openssl pkcs12 -export -out "$CB_NAME/cert.pfx" -inkey "$CB_NAME/privkey.pem" -in "$CB_NAME/cert.pem" -certfile "$CB_NAME/fullchain.pem" -passout pass:1234
+echo "creating certificate and key pem"
+cat "$CB_NAME/cert.pem" >  "$CB_NAME/certkey.pem"
+cat "$CB_NAME/privkey.pem" >> "$CB_NAME/certkey.pem"
+echo "creating fullchain and key pem"
+cat "$CB_NAME/fullchain.pem" >  "$CB_NAME/fullchainkey.pem"
+cat "$CB_NAME/privkey.pem" >> "$CB_NAME/fullchainkey.pem"
