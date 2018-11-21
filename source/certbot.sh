1418fdfa50887cc307b38d9187d6ab0c3e7f312f
11notes <github@11notes.ch>
2018-11-21 11:36:41 +0100

    based on 11notes/nginx:latest

--- source/certbot.sh
+++ source/certbot.sh
@@ -5,9 +5,7 @@ echo "creating certificate for $3"
 certbot certonly --cert-name "$1" --non-interactive --agree-tos --email "$2" --keep --webroot -w /var/www $3
 cp /etc/letsencrypt/live/"$1" -RL /var/ssl
 openssl pkcs12 -export -out "$CB_NAME/cert.pfx" -inkey "$CB_NAME/privkey.pem" -in "$CB_NAME/cert.pem" -certfile "$CB_NAME/fullchain.pem" -passout pass:1234
-echo "creating certificate and key pem"
 cat "$CB_NAME/cert.pem" >  "$CB_NAME/certkey.pem"
 cat "$CB_NAME/privkey.pem" >> "$CB_NAME/certkey.pem"
-echo "creating fullchain and key pem"
 cat "$CB_NAME/fullchain.pem" >  "$CB_NAME/fullchainkey.pem"
 cat "$CB_NAME/privkey.pem" >> "$CB_NAME/fullchainkey.pem"
